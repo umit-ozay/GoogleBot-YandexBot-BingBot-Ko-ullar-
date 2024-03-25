8e2142b595d9cd57dda820a361cd8922e78c43a9
Ümit Özay <umitozay95@gmail.com>
2024-03-25 14:17:23 +0300

    Update bots.php

--- bots.php
+++ bots.php
@@ -1,9 +1,13 @@
 <?php
-$userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-if (strpos($userAgent, 'Googlebot') !== false && strpos($userAgent, 'YandexBot') !== false && strpos($userAgent, 'bingbot') !== false) {
-    echo "Googlebot, YandexBot ve Bingbot geldi!";
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    if (strpos($userAgent, 'Googlebot') !== false || strpos($userAgent, 'YandexBot') !== false || strpos($userAgent, 'bingbot') !== false) { ?>
+ // googlebot veya yandexbot veya bingbot geldi..
+ 
+<?php } else { ?>
+      // bot dışında gelen veri.
+    <?php }
 } else {
-    echo "Bu koşulu sağlayan botlar gelmedi.";
+  // bot dışında gelen veri.
 }
 ?>
